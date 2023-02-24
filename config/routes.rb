29906b074cad586b5664ac623cb0604712762a9e
EC2 Default User <chyou726@outlook.jp>
2023-02-24 05:54:52 +0000

    編集画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'homes#top'
-  resources :books, only: [:new, :create, :index, :show]
-
+  resources :books, only: [:new, :create, :index, :show, :edit]
+  patch 'books/:id' => 'books#update', as: 'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 

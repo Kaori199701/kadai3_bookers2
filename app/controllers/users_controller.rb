8601f77b1bd04eb7f35ebad60093226935db75e5
EC2 Default User <chyou726@outlook.jp>
2023-03-01 13:02:20 +0000

    レイアウト整え1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
@@ -22,6 +23,8 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @user = current_user
+    @book = Book.new
   end
 
   private
